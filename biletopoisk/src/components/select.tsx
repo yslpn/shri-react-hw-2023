a13b39752b3849b297c2c919e65ec76733cd5395
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2023-06-22 16:30:30 +0400

    add handle key down for firs option

--- biletopoisk/src/components/select.tsx
+++ biletopoisk/src/components/select.tsx
@@ -65,7 +65,18 @@ export const Select = ({
     }
   }, []);
 
-  const handleKeyDown = useCallback(
+  const handleKeyDownFirst = useCallback(
+    (event: React.KeyboardEvent<HTMLButtonElement>) => {
+      if (event.key === "Tab" && event.shiftKey) {
+        event.preventDefault();
+        toggleButtonRef.current?.focus();
+        setIsOpen(false);
+      }
+    },
+    []
+  );
+
+  const handleKeyDownLast = useCallback(
     (event: React.KeyboardEvent<HTMLButtonElement>) => {
       if (event.key === "Tab" && !event.shiftKey) {
         event.preventDefault();
@@ -91,31 +102,34 @@ export const Select = ({
   }, [isOpen]);
 
   return (
-    <div className="relative" ref={selectWrapperRef}>
-      <label className="flex flex-col">{label}</label>
-      <button
-        className="flex justify-between items-center w-full mt-1 mb-4 py-2 px-4 rounded-lg border border-gray-lighter cursor-pointer transition hover:border-orange"
-        onClick={() => setIsOpen((s) => !s)}
-        ref={toggleButtonRef}
-      >
-        {currentOption?.label || (
-          <span className="text-gray-light">{emptyLabel}</span>
-        )}
-        <svg
-          className={`transition ${isOpen ? "rotate-180" : ""}`}
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          fill="none"
+    <>
+      <div className="relative" ref={selectWrapperRef}>
+        <label className="flex flex-col">{label}</label>
+        <button
+          className="flex justify-between items-center w-full mt-1 mb-4 py-2 px-4 rounded-lg border border-gray-lighter cursor-pointer transition hover:border-orange"
+          onClick={() => setIsOpen((s) => !s)}
+          ref={toggleButtonRef}
         >
-          <path
-            fill="#999FA6"
-            fillRule="evenodd"
-            d="M7.5 18.958h5c4.525 0 6.458-1.933 6.458-6.458v-5c0-4.525-1.933-6.458-6.458-6.458h-5c-4.525 0-6.458 1.933-6.458 6.458v5c0 4.525 1.933 6.458 6.458 6.458ZM2.292 7.5c0-3.842 1.366-5.208 5.208-5.208h5c3.842 0 5.208 1.366 5.208 5.208v5c0 3.842-1.366 5.208-5.208 5.208h-5c-3.842 0-5.208-1.366-5.208-5.208v-5Zm7.266 4.742a.618.618 0 0 0 .442.183.618.618 0 0 0 .442-.183L13.383 9.3a.629.629 0 0 0 0-.883.629.629 0 0 0-.883 0l-2.5 2.5-2.5-2.5a.629.629 0 0 0-.883 0 .629.629 0 0 0 0 .883l2.941 2.942Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
+          {currentOption?.label || (
+            <span className="text-gray-light">{emptyLabel}</span>
+          )}
+          <svg
+            className={`transition ${isOpen ? "rotate-180" : ""}`}
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="none"
+          >
+            <path
+              fill="#999FA6"
+              fillRule="evenodd"
+              d="M7.5 18.958h5c4.525 0 6.458-1.933 6.458-6.458v-5c0-4.525-1.933-6.458-6.458-6.458h-5c-4.525 0-6.458 1.933-6.458 6.458v5c0 4.525 1.933 6.458 6.458 6.458ZM2.292 7.5c0-3.842 1.366-5.208 5.208-5.208h5c3.842 0 5.208 1.366 5.208 5.208v5c0 3.842-1.366 5.208-5.208 5.208h-5c-3.842 0-5.208-1.366-5.208-5.208v-5Zm7.266 4.742a.618.618 0 0 0 .442.183.618.618 0 0 0 .442-.183L13.383 9.3a.629.629 0 0 0 0-.883.629.629 0 0 0-.883 0l-2.5 2.5-2.5-2.5a.629.629 0 0 0-.883 0 .629.629 0 0 0 0 .883l2.941 2.942Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+
       {isOpen &&
         createPortal(
           <div
@@ -131,6 +145,7 @@ export const Select = ({
               className="flex w-full py-2 px-4 cursor-pointer hover:bg-gray-200 transition hover:text-orange"
               onClick={() => handleSelectChange("")}
               ref={firstButtonRef}
+              onKeyDown={handleKeyDownFirst}
             >
               Не выбрано
             </button>
@@ -142,7 +157,7 @@ export const Select = ({
                   onClick={() => handleSelectChange(option.value)}
                   onKeyDown={
                     option === options[options.length - 1]
-                      ? handleKeyDown
+                      ? handleKeyDownLast
                       : undefined
                   }
                 >
@@ -153,6 +168,6 @@ export const Select = ({
           </div>,
           document.body
         )}
-    </div>
+    </>
   );
 };
